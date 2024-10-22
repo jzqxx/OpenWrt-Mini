ab836365bc815ffd740ae353dc204279639cac3f
jzqxx <126733798+jzqxx@users.noreply.github.com>
2024-10-22 20:46:47 +0800

    Add files via upload

--- DIY-2.sh
+++ DIY-2.sh
@@ -5,5 +5,8 @@
 rm -rf feeds/packages/lang/golang
 git clone https://github.com/kenzok8/golang feeds/packages/lang/golang
 
-# 2-设置密码为空
+# 2-设置管理地址
+sed -i 's/192.168.1.1/192.168.123.254/g' package/base-files/files/bin/config_generate
+
+# 3-设置密码为空
 sed -i '/CYXluq4wUazHjmCDBCqXF/d' package/lean/default-settings/files/zzz-default-settings
